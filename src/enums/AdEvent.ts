33f4acc178d585a1bef39d98507c908b012621fc
Dev Fred <manfredtochukwu362@gmail.com>
2024-06-01 14:08:20 +0100

    "Replace AdEvent enum with union type"

--- src/enums/AdEvent.ts
+++ src/enums/AdEvent.ts
@@ -1,10 +1,7 @@
-/**
- * Enum for Ad events.
- */
-export enum AdEvent {
-    reward = 'onReward',
-    skip = 'onSkip',
-    error = 'onError',
-    bannerNotFound = 'onBannerNotFound',
-    start = 'onStart',
-}
+// AdEvent.ts
+export type AdEvent =
+    | 'onReward'
+    | 'onSkip'
+    | 'onError'
+    | 'onBannerNotFound'
+    | 'onStart';
